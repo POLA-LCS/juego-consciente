c7909704061b25a7239f6a073feb61fff6df81c1
POLA-LCS <baltazarpilling10@gmail.com>
2025-11-15 08:54:41 -0300

    Se cambiaron las practicas de `index.php`

--- index.php
+++ index.php
@@ -5,38 +5,54 @@ define('ROOT_PATH', __DIR__ . '/');
 
 if (isset($_GET['action'])) {
     include 'app/controllers/UserController.php';
-} else {
-    $page = isset($_GET['page']) ? $_GET['page'] : 'login';
-
-    $protected_pages = ['account', 'dashboard', 'contact', 'info', 'blackjack', 'cups', 'roulette', 'slots'];
-
-    $routes = [
-        'login'     => 'app/views/login.php',
-        'register'  => 'app/views/register.php',
-        'dashboard' => 'app/views/dashboard.php',
-        'account'   => 'app/views/account.php',
-        'contact'   => 'app/views/contact.php',
-        'info'      => 'app/views/info.php',
-        'blackjack' => 'app/views/games/blackjack.php',
-        'cups'      => 'app/views/games/cups.php',
-        'roulette'  => 'app/views/games/roulette.php',
-        'slots'     => 'app/views/games/slots.php',
-        'error403'  => 'app/views/errors/403.php',
-        'error404'  => 'app/views/errors/404.php',
-    ];
-
-    if (in_array($page, $protected_pages) && !isset($_SESSION['user_id'])) {
-        $page = 'login';
-    }
-
-    if (array_key_exists($page, $routes)) {
-        if ($page === 'error403') {
-            http_response_code(403);
-        }
-
-        include $routes[$page];
-    } else {
-        http_response_code(404);
-        include 'app/views/errors/404.php';
-    }
+    exit();
 }
+
+// Rutas protegidas
+$protected_pages = [
+    'account',
+    'dashboard',
+    'contact',
+    'info',
+    'blackjack',
+    'cups',
+    'roulette',
+    'slots'
+];
+
+// Rutas validas
+$routes = [
+    'register'  => 'app/views/register.php',
+    'login'     => 'app/views/login.php',
+    'dashboard' => 'app/views/dashboard.php',
+    'account'   => 'app/views/account.php',
+    'info'      => 'app/views/info.php',
+    'contact'   => 'app/views/contact.php',
+    'blackjack' => 'app/views/games/blackjack.php',
+    'cups'      => 'app/views/games/cups.php',
+    'roulette'  => 'app/views/games/roulette.php',
+    'slots'     => 'app/views/games/slots.php',
+    'ERROR_403'  => 'app/views/errors/403.php',
+];
+
+// Entrada inicial
+$page = isset($_GET['page']) ? $_GET['page'] : 'register';
+
+// Verificar si la página es protegida y si el usuario no está autenticado
+if (in_array($page, $protected_pages) && !isset($_SESSION['user_id'])) {
+    $page = 'login';
+}
+
+// Pagina no existente
+if (!array_key_exists($page, $routes)) {
+    http_response_code(404);
+    include 'app/views/errors/404.php';
+    exit();
+}
+
+if ($page === 'ERROR_403') {
+    http_response_code(403);
+}
+
+include $routes[$page];
+exit();
